5076525ddf93466c6fa96ce0e9a403f2b64930f5
sriman <kssrisowrirajan@gmail.com>
2023-04-26 01:57:03 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import heart from '../assets/heart.svg';
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      Made with
+      <img
+      className = {styles.heart}
+       src={heart} />
+      by Prodigy Webops
+    </div>
+  );
 };
 
 export default Footer;

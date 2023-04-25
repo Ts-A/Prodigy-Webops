9dba8255d673879c3a05cdf803480acffcdd5adc
Ganishk <ganishk1@gmail.com>
2023-04-25 20:27:38 +0530

    added more content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
-
+import styles from './styles/Home.module.css';
 
 const About = () => {
   return (
-    <div className="container" id="about">
+    <div className={styles.container} id="about">
       <h1 id="heading">ABOUT US</h1>
       <div>
         <p>Prodigy, the annual symposium of the Production department, is back again, offering a refreshing set of events, workshops, guest lectures, and quizzes that will keep your minds sharp and hone your skills. A student-run organization that aims at encouraging technological innovation and the development of managerial skills, Prodigy'22 allows you a chance to test your expertise by competing with fellow prodigies and finding out where you stand. With outreach programs and events conducted in various cities throughout the year, months of hard work reach a crescendo in the last two days, attracting more than 1000 students from various other colleges as Prodigy strives to break boundaries and test the limits of the technological world. With each passing edition, Prodigy refines itself to become even better. Prodigy'22 attempts to break its own record and promises nothing short of a phenomenal experience that will leave you amazed at the marvels of technology.
           </p>
+	  <p>Since its inception in 1982, Prodigy has encouraged technical innovation and the development of managerial skills among the student community. Educative workshops, guest lectures from distinguished personalities in tech, panel discussions with industry experts, mind-boggling quizzes and much more await you at the three-day technical extravaganza to help you upskill and gain insights into the field of production engineering.
+	  </p>
       </div>
     </div>
   )

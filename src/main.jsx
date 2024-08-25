4ce84df9090a08cdb422c330b064190bd21697b6
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-08-25 15:04:48 +0530

    remove routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,6 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home";
 
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-import Home from './pages/Home';
-import Teams from './pages/Teams';
-import Gallery from './pages/Gallery';
-
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/teams' element={<Teams />} />
-        <Route path='/gallery' element={<Gallery />} />
-      </Routes>
-    </BrowserRouter>
-    <Footer />
-  </>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<Home />);

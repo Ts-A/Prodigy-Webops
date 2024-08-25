b523daefaa5e5730352b7ee2318366e3dab27ab0
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-08-25 15:17:59 +0530

    magicui and tailwindcss integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import BlurIn from "@/components/magicui/blur-in";
 import React, { useRef } from "react";
 import About from "./About";
 import Contacts from "./Contacts";
@@ -7,16 +8,13 @@ import styles from "./styles/Home.module.css";
 const Home = () => {
   const about = useRef(null);
 
-  const HandleScroll = () => {
-    about.current.scrollIntoView({ behavior: "smooth" });
-  };
-
   return (
     <container>
       <div className={styles.title}>
         <h1 id={styles.prodigy}>PRODIGY</h1>
         <img src="/assets/icons/arrow.svg"></img>
       </div>
+      <BlurIn word={"Prodigy"}></BlurIn>
       <About ref={about} />
       <Events />
       <Contacts />

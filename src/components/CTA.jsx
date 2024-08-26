37d8fb852a8443a37c847aca17dbdb7d123960c2
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-08-26 14:16:45 +0530

    #5: simple contact

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,8 +2,8 @@ import PulsatingButton from "./ui/pulsating-button";
 
 const CTA = () => {
   return (
-    <div className="text-center">
-      <span className="text-lg">For more queries</span>
+    <div className="text-center bg-indigo-500">
+      <span className="text-xl">For more queries</span>
       <a href="mailto:prodigy@nitt.edu">
         <PulsatingButton className={"ml-3 inline-block"}>
           Contact Us

139cf1201d8c6f6e3f894da9f2b3f96809a81965
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-08-29 13:33:02 +0530

    hero

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -1,5 +1,12 @@
 const Hero = () => {
-  return <div className="text-pr-p bg-pr-s bungee-regular">Hero</div>;
+  return (
+    <div className="border bg-pr-p text-pr-s">
+      <div className="text-center bungee-regular text-[100px]">Prodigy</div>
+      <div className="text-center font-semibold text-[20px]">
+        where innovation meets excellence
+      </div>
+    </div>
+  );
 };
 
 export default Hero;

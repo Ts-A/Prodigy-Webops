d19b4bd2e2eabde1ecde8d6f5989b1ee072f6980
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-08-29 13:20:26 +0530

    navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,5 +1,13 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-[70px] flex px-20 items-center mb-[70px] bg-pr-t">
+      <div className="grow text-2xl cursor-pointer">Prodigy</div>
+      <div className="flex gap-7">
+        <nav className="cursor-pointer">Schedule</nav>
+        <nav className="cursor-pointer">Contact</nav>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

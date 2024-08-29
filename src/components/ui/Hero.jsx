b031cd0a39c9494278e7c50b054e778773429e6f
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-08-29 14:34:56 +0530

    testimonial section

--- src/components/ui/Hero.jsx
+++ src/components/ui/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="border bg-pr-p text-pr-s">
+    <div className="bg-pr-p text-pr-s">
       <div className="text-center bungee-regular text-[100px]">Prodigy</div>
       <div className="text-center font-semibold text-[20px]">
         where innovation meets excellence
--- src/components/ui/Testimonial.jsx
+++ src/components/ui/Testimonial.jsx
@@ -1,5 +1,125 @@
-const Testimonial = () => {
-  return <div className="text-pr-p bg-pr-s bungee-regular">Testimonial</div>;
+import Marquee from "@/components/magicui/marquee";
+import { cn } from "@/lib/utils";
+
+const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://avatar.vercel.sh/jack",
+    gender: "male",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://avatar.vercel.sh/jill",
+    gender: "female",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/john",
+    gender: "female",
+  },
+  {
+    name: "Jane",
+    username: "@jane",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jane",
+    gender: "female",
+  },
+  {
+    name: "Jenny",
+    username: "@jenny",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jenny",
+    gender: "male",
+  },
+  {
+    name: "James",
+    username: "@james",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/james",
+    gender: "male",
+  },
+  {
+    name: "Jane",
+    username: "@jane",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jane",
+    gender: "female",
+  },
+  {
+    name: "Jenny",
+    username: "@jenny",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/jenny",
+    gender: "male",
+  },
+  {
+    name: "James",
+    username: "@james",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://avatar.vercel.sh/james",
+    gender: "male",
+  },
+];
+
+const firstRow = reviews.slice(0, reviews.length / 2);
+const secondRow = reviews.slice(reviews.length / 2);
+
+const ReviewCard = ({ img, name, username, body, gender }) => {
+  return (
+    <figure
+      className={cn(
+        "bg-pr-s text-pr-p relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+        "border-gray-950/[.1] hover:bg-pr-p hover:text-pr-s"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <img
+          className="rounded-full"
+          width="32"
+          height="32"
+          alt="avatar"
+          src={
+            "https://avatar.iran.liara.run/public/" +
+            (gender === "male"
+              ? "boy?username=" + username
+              : "girl?username=" + username)
+          }
+        />
+        <div className="flex flex-col">
+          <figcaption className="text-sm font-medium">{name}</figcaption>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-white text-sm">{body}</blockquote>
+    </figure>
+  );
 };
 
-export default Testimonial;
+const Testimomial = () => {
+  return (
+    <div>
+      <div className="text-center text-[40px] bg-pr-p text-pr-s font-semibold my-5">
+        Hear what they say about us!
+      </div>
+      <div className="relative flex w-9/12 mx-auto px-10 flex-col items-center justify-center overflow-hidden bg-pr-t">
+        <Marquee pauseOnHover className="my-5 [--duration:20s]">
+          {firstRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+        <Marquee reverse pauseOnHover className="my-5 [--duration:20s]">
+          {secondRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+      </div>
+    </div>
+  );
+};
+
+export default Testimomial;

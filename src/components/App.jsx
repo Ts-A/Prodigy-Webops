a55da671c325699f4713f957b986db66a1da1ea9
T S Ajeet <ajeet.tumuluru@gmail.com>
2024-08-29 12:54:55 +0530

    update tailwind colours #12

---
+++ src/components/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-pr-p bg-pr-s bungee-regular">Hello World</div>;
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
     },
     extend: {
       colors: {
+        "pr-p": "var(--pr-p)",
+        "pr-s": "var(--pr-s)",
+        "pr-t": "var(--pr-t)",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
